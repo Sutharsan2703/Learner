dfb170a91a7a131d8496e7109a602043d02439c7
suth3338 <suth3338@CSLH3147.global.chainsys.com>
2022-11-17 12:01:09 +0530

    Array

--- com/chainsys/day5/Array.java
+++ com/chainsys/day5/Array.java
@@ -3,8 +3,13 @@ package com.chainsys.day5;
 public class Array {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int empid;
+		String name;
+		String dest;
+		
+		emp() {}
+		
+		
 	}
 
 }

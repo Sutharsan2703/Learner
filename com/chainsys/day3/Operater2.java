69f5e20a029c0bf52a7c239d6c752f2919e89041
suth3338 <suth3338@CSLH3147.global.chainsys.com>
2022-11-17 11:40:33 +0530

    check

---
+++ com/chainsys/day3/Operater2.java
@@ -0,0 +1,22 @@
+package com.chainsys.day3;
+
+import java.util.Scanner;
+
+public class Operater2 {
+
+	public static void main(String[] args) {
+		//Unary -,++,--
+		//x=5
+		System.out.println("Enter Any Number: ");
+		Scanner sc = new Scanner(System.in);
+		int x =sc.nextInt();
+		System.out.println(x++);	//(x=x+1->x-5(+1)//5
+		System.out.println(x);		//6
+		System.out.println(++x);	//x=1+x=->1+6//7
+		System.out.println(x--);	//7
+		System.out.println(x);		//6
+		System.out.println(--x);	//5
+		System.out.println(x);		//5
+	}
+
+}
